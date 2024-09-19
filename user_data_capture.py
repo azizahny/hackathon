c4709f33372ee0d578450d115fa034dd78b85e3a
azizahny <35856027+azizahny@users.noreply.github.com>
2024-09-19 12:06:46 +0700

    Update and rename user_data_capture to user_data_capture.py

---
+++ user_data_capture.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from hugchat import hugchat
+from hugchat.login import Login
+
+# App title
+st.set_page_config(page_title="💬 Cakap Assistant")
+    
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "How may I help you?"}]
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
